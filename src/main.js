0b50f348e02f1358ca1740163239c56e4a76bd51
Klaudia Witman <claudiawitman@gmail.com>
2024-09-11 15:37:58 +0100

    JS added. Timezones to homepage

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
+function updatetime() {
+  //London
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
 
+  let londonZone = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londonZone.format("LL");
+  londonTimeElement.innerHTML = londonZone.format("h:m:sa");
+
+  //Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+
+  let tokyoZone = moment().tz("Asia/Tokyo");
+  tokyoDateElement.innerHTML = tokyoZone.format("LL");
+  tokyoTimeElement.innerHTML = tokyoZone.format("h:m:sa");
+
+  //Warsaw
+  let warsawElement = document.querySelector("#warsaw");
+  let warsawDateElement = warsawElement.querySelector(".date");
+  let warsawTimeElement = warsawElement.querySelector(".time");
+
+  let warsawZone = moment().tz("Europe/Warsaw");
+  warsawDateElement.innerHTML = warsawZone.format("LL");
+  warsawTimeElement.innerHTML = warsawZone.format("h:m:sa");
+}
+updatetime();
+setInterval(updatetime, 1000);
